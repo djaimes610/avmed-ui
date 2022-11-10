1ddae63c248f6ce046ddb51f2009cf31ed982019
Gaby Marapi <gmarapi@farmaciasperuanas.pe>
2022-11-10 17:45:43 -0500

    fix print prescription

--- src/app/business/prescription/prescription.component.ts
+++ src/app/business/prescription/prescription.component.ts
@@ -9,14 +9,7 @@ import { NgxPrintElementService } from 'ngx-print-element';
   styleUrls: ['./prescription.component.css']
 })
 export class PrescriptionComponent implements OnInit {
-  public config = {
-    printMode: 'template-popup',
-    popupProperties: 'toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=0,fullscreen=yes',
-    pageTitle: 'Hello World',
-    templateString: '<header>I\'m part of the template header</header>{{printBody}}<footer>I\'m part of the template footer</footer>',
-    stylesheets: [{ rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' }],
-    styles: ['td { border: 1px solid black; color: green; }', 'table { border: 1px solid black; color: red }', 'header, table, footer { margin: auto; text-align: center; }']
-  }
+
 
   @Input('attention') attentionSelected: AttentionRp = new AttentionRp('','','',this.getNewTriage(),[],[],'',this.getNewPatient(),this.getNewHealthPersonnel(),'');
 
